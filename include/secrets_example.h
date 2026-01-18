2a92d8a682154355cb33bf73a49cc31fad927199
André <andre.zempelburg@gmail.com>
2026-01-18 12:25:13 +0100

    Add secrets_example.h template

---
+++ include/secrets_example.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Secrets {
+  // OpenAI API key
+  static const char* OpenAI = "YOUR_OPENAI_API_KEY";
+
+  // OpenWeather API key
+  static const char* OpenWeather = "YOUR_OPENWEATHER_API_KEY";
+}
